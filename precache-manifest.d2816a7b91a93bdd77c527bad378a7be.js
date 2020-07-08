be6c0b0e99cb5587db5f089aeb4d43dcab3ab4b8
Konstantyn Savenkov <ksavenkov@qarea.com>
2020-07-08 03:03:14 +0300

    Updates

---
+++ precache-manifest.d2816a7b91a93bdd77c527bad378a7be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63f9fd80444a86311d0c79d43b2c4e9",
+    "url": "/Skill-Led/player-music-online/index.html"
+  },
+  {
+    "revision": "475bb9058d4cbab4400e",
+    "url": "/Skill-Led/player-music-online/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08c123b4c1dc48efc7f7",
+    "url": "/Skill-Led/player-music-online/static/js/2.fcf9fbde.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Skill-Led/player-music-online/static/js/2.fcf9fbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475bb9058d4cbab4400e",
+    "url": "/Skill-Led/player-music-online/static/js/main.84513612.chunk.js"
+  },
+  {
+    "revision": "16487852229b0a6737d5",
+    "url": "/Skill-Led/player-music-online/static/js/runtime-main.79045127.js"
+  }
+]);
